ed195f45cca98065d759c47602a34f6835f640a0
Oleksandr <o.vynogrado@epages.com>
2016-03-09 10:21:23 +0100

    Extracted developer specific setting. Moved files to templates.

---
+++ init_settings.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+export SETTINGS="$1"
+
+if [ -z "$1" ]
+  then
+  echo ""
+  echo "============================================================================================================"
+  echo "Misconfiguration! Specify location of file to store personalised settings as the script parameter. For instance ./.personalised_settings.sh"
+  echo "============================================================================================================"
+  echo ""
+  return 1
+fi
+
+if [ -f $SETTINGS ]
+then 
+   echo "File containing pelsonalised settings already present. See $SETTINGS"
+   return 0
+fi
+
+echo ""
+echo "============================================================================================================"
+echo "To initialize the provisioning scripts please input yor data, which will be stored $SETTINGS"
+echo "============================================================================================================"
+echo ""
+
+read -p "Please specify your username for this machine:" DEV_USERNAME
+read -p "Please specify your email:" DEV_EMAIL
+read -p "Please specify your first and last name :" DEV_FULLNAME
+
+echo "#!/bin/bash" > $SETTINGS
+echo "export DEV_USERNAME=$DEV_USERNAME" >> $SETTINGS
+echo "export DEV_EMAIL=$DEV_EMAIL" >> $SETTINGS
+echo "export DEV_FULLNAME=$DEV_FULLNAME" >> $SETTINGS
+
+echo ""
+echo "============================================================================================================"
+echo " Entered data is stored in $SETTINGS"
+echo "============================================================================================================"
+echo ""
